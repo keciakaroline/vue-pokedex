e00510ae5279e1960307c12b8143daea2f9c774c
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-09-30 01:20:49 -0300

    feat: Added router configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Pokedex from "@/pages/Pokedex.vue";
+import PokemonDetails from "@/pages/PokemonDetails.vue";
+import { createPinia } from "pinia";
 
+const routes = [
+  {
+    path: "/",
+    name: "Pokedex",
+    component: Pokedex,
+  },
+  {
+    path: "/pokemon/:id",
+    name: "PokemonDetails",
+    component: PokemonDetails,
+    props: true,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const pinia = createPinia();
 const app = createApp(App);
-app.use(createPinia());
+
+app.use(pinia);
+app.use(router);
+
 app.mount("#app");
