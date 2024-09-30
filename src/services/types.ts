8384bb20cf5e6f5311e0c7f64899e7c6a80bbdb8
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-09-29 23:45:06 -0300

    refactor: Update Pokemon and Pokedex state interfaces

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,10 +1,54 @@
 interface Pokemon {
+  id: number;
   name: string;
-  url: string;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+  types: { type: { name: string } }[];
+  stats: { stat: { name: string }; base_stat: number }[];
+  weight: number;
+  height: number;
+  abilities: { ability: { name: string } }[];
 }
 
 interface PokemonResponse {
   results: Pokemon[];
 }
 
-export type { Pokemon, PokemonResponse };
+interface PokemonSpecie {
+  flavor_text_entries: { flavor_text: string; language: { name: string } }[];
+}
+
+interface PokemonState {
+  pokemon: Pokemon | null;
+  pokemonSpecie: PokemonSpecie | null;
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+  isLoadingSpecie: boolean;
+  isErrorSpecie: boolean;
+  errorSpecie: string | null;
+}
+
+interface PokedexState {
+  search: string;
+  searchMode: "name" | "id";
+  currentPage: number;
+  filter: Pokemon[];
+  pokemons: Pokemon[];
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+}
+
+export type {
+  Pokemon,
+  PokemonResponse,
+  PokemonSpecie,
+  PokemonState,
+  PokedexState,
+};
