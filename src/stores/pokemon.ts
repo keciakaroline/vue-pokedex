0a7bd42ae54d3587c3d711810d51b7fbb3dde162
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-09-30 18:32:21 -0300

    feat: Add Pokemon store and actions for fetching Pokemon data

---
+++ src/stores/pokemon.ts
@@ -0,0 +1,54 @@
+import { defineStore } from "pinia";
+import {
+  getPokemonByName,
+  getPokemonSpecies,
+} from "@/services/pokemonApiService";
+import { PokemonState } from "@/services/types";
+
+export const usePokemonStore = defineStore("pokemon", {
+  state: (): PokemonState => ({
+    pokemon: null,
+    pokemonSpecie: null,
+    isLoading: false,
+    isError: false,
+    error: null,
+    isLoadingSpecie: false,
+    isErrorSpecie: false,
+    errorSpecie: null,
+  }),
+
+  actions: {
+    async fetchPokemonByName(name: string) {
+      this.isLoading = true;
+      this.isError = false;
+      this.error = null;
+
+      try {
+        const response = await getPokemonByName(name);
+        this.pokemon = response.data;
+      } catch (error: any) {
+        this.isError = true;
+        this.error = error.message || "Erro ao buscar o Pokémon.";
+      } finally {
+        this.isLoading = false;
+      }
+    },
+
+    async fetchPokemonSpecies(name: string) {
+      this.isLoadingSpecie = true;
+      this.isErrorSpecie = false;
+      this.errorSpecie = null;
+
+      try {
+        const response = await getPokemonSpecies(name);
+        this.pokemonSpecie = response.data;
+      } catch (error: any) {
+        this.isErrorSpecie = true;
+        this.errorSpecie =
+          error.message || "Erro ao buscar a espécie do Pokémon.";
+      } finally {
+        this.isLoadingSpecie = false;
+      }
+    },
+  },
+});
