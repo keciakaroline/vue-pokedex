2c67fd6487183ff0e9c3df54f07796384de0c54f
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-10-03 15:43:42 -0300

    env: Remove unused mock server and cleanup test setup

--- src/test/mockServer.ts
+++
@@ -1,7 +0,0 @@
-import { setupServer } from "msw/node";
-
-const mockServer = setupServer();
-
-const API_URL = "https://pokeapi.co/api/v2/";
-
-export { mockServer, API_URL };
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,21 +1,6 @@
-import { afterAll, afterEach, beforeAll } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/vue";
-import { mockServer } from "./mockServer";
-
-beforeAll(() => {
-  mockServer.listen({
-    onUnhandledRequest: "error",
-  });
-});
-
-afterEach(() => {
-  mockServer.resetHandlers();
-});
 
 afterEach(() => {
   cleanup();
-});
-
-afterAll(() => {
-  mockServer.close();
 });
