5c918df5ac91da846d962e207e2926c601a3a110
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-10-03 14:13:09 -0300

    test: Add unit test for PokemonCard.vue component

---
+++ src/test/components/PokemonCard.test.ts
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+import PokemonCard from "@/components/PokemonCard.vue";
+import "@testing-library/jest-dom";
+
+describe("PokemonCard.vue", () => {
+  it("deve renderizar o nome, id formatado e imagem do Pokémon corretamente", () => {
+    const props = {
+      name: "pikachu",
+      id: 25,
+      sprites:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    };
+
+    render(PokemonCard, { props });
+    const img = screen.getByAltText("pikachu");
+
+    expect.soft(screen.getByText("pikachu")).toBeInTheDocument();
+    expect.soft(screen.getByText("#025")).toBeInTheDocument();
+    expect.soft(img).toBeInTheDocument();
+    expect.soft(img).toHaveAttribute("src", props.sprites);
+  });
+});
